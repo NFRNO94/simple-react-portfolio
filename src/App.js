8ab3099b7cacf81c30fb778c93583ee026bf6368
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-09-30 19:28:06 -0700

    removed app css, as rendering issue was not resolved

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Statement from "../src/components/Statement.js";
 import Img from "../src/assets/images/landBack.png";
 // import { fadeIn, rotateOut } from 'react-animations';
 import styled, { keyframes } from 'styled-components';
-import './App.css';
 
 const AppStyle = createGlobalStyle`
   html, body {
@@ -19,6 +18,10 @@ const AppStyle = createGlobalStyle`
     height: 100%;
     color: white;
     scroll-behavior: smooth;
+    background-image: url(${Img});
+    background-size: cover;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
   }
 `;
 
